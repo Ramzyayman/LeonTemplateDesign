97f9771c2c986bbe3fd30cd064b969fa5f762516
Ramzy Ayman <ramzyayman13@gmail.com>
2022-11-21 06:29:52 +0200

    Upload files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let links_button = document.querySelector('.links')
+let droplist = document.querySelector('#droplist')
+
+
+links_button.onclick = function () {
+
+    droplist.classList.toggle('hidden')
+}
